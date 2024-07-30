805bc8f248ec434f9ad64b5e166c70afc6100a9f
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-07-30 08:33:46 -0300

    commit 02

--- SiteUno/src/components/Historia.jsx
+++ SiteUno/src/components/Historia.jsx
@@ -1,32 +1,32 @@
 export default function Historia() {
     return(
-        <div className="containerHI">
-            <div className="HI01">
-                <div>
+        <div className="containerCI">
+            <div className="C01">
+                <div className="HI01">
                     <h1>Historia</h1>
                     <p>O jogo de cartas Uno foi criado por Merle Robbins em 1971. Robbins, um barbeiro de Reading, Ohio, inventou o jogo como uma variação do jogo de cartas tradicional Crazy Eights. Ele e sua família começaram a jogar Uno, e o jogo rapidamente se tornou popular entre seus amigos e vizinhos.</p>
                 </div>
-                <div>
+                <div className="HI01">
                     <h1>Criação e Lançamento Inicial</h1>
                     <p>Merle Robbins investiu $8,000 para fabricar 5,000 cópias do jogo, que ele começou a vender em sua barbearia e, posteriormente, em lojas locais. O jogo rapidamente ganhou popularidade, e em 1972, Robbins vendeu os direitos de Uno para um grupo de empresários liderado por Robert Tezak, dono de uma funerária em Joliet, Illinois, por $50,000 mais royalties de $0.10 por jogo vendido.</p>
                 </div>
-                <div>
+                <div className="HI01">
                     <h1>Evolução e Popularidade</h1>
                     <p>Tezak fundou a International Games, Inc. para comercializar Uno. O jogo continuou a crescer em popularidade e foi eventualmente adquirido pela Mattel em 1992. A partir daí, Uno se tornou um dos jogos de cartas mais vendidos e amplamente reconhecidos no mundo.</p>
                 </div>
-                <div>
+                <div className="HI01">
                     <h1>Variantes e Expansões</h1>
                     <p>Ao longo dos anos, Uno viu várias versões e expansões. Existem edições temáticas baseadas em filmes, programas de TV e personagens populares, além de variantes com regras adicionais e cartas especiais. Algumas das versões mais populares incluem Uno Attack, Uno Spin e Uno Dare.</p>
                 </div>
-                <div>
+                <div className="HI01">
                     <h1>Impacto Cultural</h1>
                     <p>Uno não só se manteve popular ao longo das décadas, mas também se tornou parte da cultura pop, sendo jogado por pessoas de todas as idades em todo o mundo. Sua simplicidade e o equilíbrio entre sorte e estratégia contribuem para seu apelo duradouro.</p>
                 </div>
-                <div>
+                <div className="HI01">
                     <h1>Adaptabilidade</h1>
                     <p>Uno também se adaptou aos tempos modernos com versões digitais disponíveis em várias plataformas, incluindo consoles de videogame, smartphones e computadores. Isso ajudou a manter o jogo relevante e acessível para as novas gerações.</p>
                 </div>
-                <div>
+                <div className="HI01">
                     <h1>Conclusão</h1>
                     <p>Desde sua criação em 1971 por Merle Robbins, Uno evoluiu de um simples jogo de cartas vendido em uma barbearia para um fenômeno global amado por milhões. Sua capacidade de inovar e se adaptar às mudanças culturais e tecnológicas assegura que Uno continuará a ser uma escolha popular para entretenimento por muitos anos.</p>
                 </div>
